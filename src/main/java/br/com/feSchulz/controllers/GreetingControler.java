fbde89475e3a53c172e489693d3cc7f89c8c32d4
Felipe Correia Schulz <zarph1994@gmail.com>
2025-10-19 11:39:53 -0300

    main
    primeiro comit do projeto 3.4.10

---
+++ src/main/java/br/com/feSchulz/controllers/GreetingControler.java
@@ -0,0 +1,18 @@
+package br.com.feSchulz.controllers;
+
+import br.com.feSchulz.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingControler {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    @RequestMapping("/greeting")
+    public Greeting greeting (@RequestParam(value="name",defaultValue = "Word") String name){
+        return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
+}
